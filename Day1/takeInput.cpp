f6a95d0b134412916a1403b3f6031dc2e6054aff
Mohit <59963561+Mohitbisht3031@users.noreply.github.com>
2024-08-14 21:04:13 +0530

    Add files via upload
    Day1

---
+++ Day1/takeInput.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int v;
+    cout<<"give an integer value : ";
+    cin>>v;
+    cout<<"********************"<<endl;
+    cout<<"Hi!! you have given "<<v<<" value as input"<<endl;
+
+    return 0;
+}
