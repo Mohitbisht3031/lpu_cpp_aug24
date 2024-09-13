15ddd45a6dd8e829af85ab9e5651f64efadd9ebc
Mohit Bisht <mohitbisht@Mohits-MacBook-Air.local>
2024-09-13 20:59:37 +0530

    Day20

---
+++ Day20/LLconti.cpp
@@ -0,0 +1,81 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class LLnode{
+    public:
+    int val;
+    LLnode*next;
+    // parametrize constructor
+    LLnode(int val) : val(val) , next(NULL){}
+};
+
+//To make the linked list
+LLnode* make_rec(){
+    int v;
+    cin>>v;
+    if(v == -1)return NULL;
+    LLnode*n = new LLnode(v);
+    n->next = make_rec();
+    return n;
+}
+LLnode* make(){
+    int v;
+    cin>>v;
+    if(v == -1)return NULL;
+    LLnode* h = new LLnode(v);
+    LLnode*t = h;
+    while(1){
+        cin>>v;
+        if(v == -1)break;
+        t->next = new LLnode(v);
+        t = t->next;
+    }
+
+    return h;
+}
+
+//Printing the linked list
+void print(LLnode*&h){
+    if(!h)return ;
+    LLnode*t = h;
+    while(t){
+        if(t->next)cout<<t->val<<"->";
+        else cout<<t->val<<"->NULL";
+        t = t->next;
+    }
+    cout<<endl;
+    return;
+}
+
+void print_rec(LLnode*h){
+    if(!h){
+        cout<<"Null";
+        return;
+    }
+    cout<<h->val<<"->";
+    print_rec(h->next);
+    return;
+}
+
+//find size of linked list
+int length(LLnode*&h){
+    LLnode*t = h;
+    int c = 0;
+    while(t){
+        c++;
+        t = t->next;
+    }
+    return c;
+}
+int length_rec(LLnode*h){
+    if(!h)return 0;
+    int l = length_rec(h->next);
+    return (l+1);
+}
+int main(){
+    LLnode* h = make_rec();
+    print_rec(h);
+    cout<<endl;
+    cout<<"size of the ll is "<<length_rec(h)<<endl;
+    return 0;
+}
